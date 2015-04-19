20c6ecd17bb8504750bfed8a43a0410327a10279
Denis Hmorinski <denishmorinski@gmail.com>
2015-04-19 14:40:21 +0200

    gallery

--- loncar.js
+++ loncar.js
@@ -1,22 +1,57 @@
-// fixed navigation
+window.onscroll = changePos;
 
-$(document).ready(function() {  
-    
-var stickyNavTop = $('.nav').offset().top;  
-var stickyNav = function(){  
-var scrollTop = $(window).scrollTop();  
-       
-if (scrollTop > stickyNavTop) {   
-    $('.nav').addClass('sticky');  
-} else {  
-    $('.nav').removeClass('sticky');   
-}  
-       
-};   
-stickyNav();  
-  
-$(window).scroll(function() {  
-    stickyNav();  
-});  
-}
-    );
+function changePos() {
+    var navigation = document.getElementById("navigation");
+    if (window.pageYOffset > 0) {
+        navigation.style.position = "fixed";
+        navigation.style.top = "0";
+    } else {
+        navigation.style.position = "";
+        navigation.style.top = "";
+    }
+}window.onscroll = changePos;
+
+
+// gallery
+$(document).ready(function() {
+    $("#single_1").fancybox({
+          helpers: {
+              title : {
+                  type : 'float'
+              }
+          }
+      });
+
+    $("#single_2").fancybox({
+    	openEffect	: 'elastic',
+    	closeEffect	: 'elastic',
+
+    	helpers : {
+    		title : {
+    			type : 'inside'
+    		}
+    	}
+    });
+
+    $("#single_3").fancybox({
+    	openEffect : 'none',
+    	closeEffect	: 'none',
+    	helpers : {
+    		title : {
+    			type : 'outside'
+    		}
+    	}
+    });
+
+    $("#single_4").fancybox({
+    	helpers : {
+    		title : {
+    			type : 'over'
+    		}
+    	}
+    });
+});
+
+$(document).ready(function() {
+				$(".fancybox").fancybox();
+			});
