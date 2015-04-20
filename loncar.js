dc7813694c0d8b9c0aa182c5d8d794dacfd61447
Denis Hmorinski <denishmorinski@gmail.com>
2015-04-20 16:15:44 +0200

    gallery

--- loncar.js
+++ loncar.js
@@ -12,46 +12,4 @@ function changePos() {
 }window.onscroll = changePos;
 
 
-// gallery
-$(document).ready(function() {
-    $("#single_1").fancybox({
-          helpers: {
-              title : {
-                  type : 'float'
-              }
-          }
-      });
 
-    $("#single_2").fancybox({
-    	openEffect	: 'elastic',
-    	closeEffect	: 'elastic',
-
-    	helpers : {
-    		title : {
-    			type : 'inside'
-    		}
-    	}
-    });
-
-    $("#single_3").fancybox({
-    	openEffect : 'none',
-    	closeEffect	: 'none',
-    	helpers : {
-    		title : {
-    			type : 'outside'
-    		}
-    	}
-    });
-
-    $("#single_4").fancybox({
-    	helpers : {
-    		title : {
-    			type : 'over'
-    		}
-    	}
-    });
-});
-
-$(document).ready(function() {
-				$(".fancybox").fancybox();
-			});
