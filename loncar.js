c8a54427b434457cf081cda93803b8c3f9ab66d5
tinvalincic <tinvalinc@gmail.com>
2015-04-21 17:53:54 +0200

    promjena_1_liz
    Here you go, my padawan

--- loncar.js
+++ loncar.js
@@ -11,5 +11,67 @@ function changePos() {
     }
 }window.onscroll = changePos;
 
+// skrivanje galerije
 
+		$(document).ready(function sakrij(){
+		  $('#klik_svi').click(function() {
+		  $('#slike_mali').animate({opacity: '0.2'}).slideUp('slow');
+		  $('#slike_veliki').animate({opacity: '0.2'}).slideUp('slow');
+		  $('#slike_dvoriste').animate({opacity: '0.2'}).slideUp('slow');
+		  $('#slike_novalja').animate({opacity: '0.2'}).slideUp('slow');
+
+		  $('#slike_svi').slideDown('slow').animate({opacity: '0.8'});
+		});
+		}); 
+		$(document).ready(function sakrij(){
+		  $('#klik_mali').click(function() {
+		  $('#slike_svi').animate({opacity: '0.2'}).slideUp('slow');
+		  $('#slike_veliki').animate({opacity: '0.2'}).slideUp('slow');
+		  $('#slike_dvoriste').animate({opacity: '0.2'}).slideUp('slow');
+		  $('#slike_novalja').animate({opacity: '0.2'}).slideUp('slow');
+
+		  $('#slike_mali').slideDown('slow').animate({opacity: '0.8'});
+		});
+		}); 
+		$(document).ready(function sakrij(){
+		  $('#klik_veliki').click(function() {
+		  $('#slike_svi').animate({opacity: '0.2'}).slideUp('slow');
+		  $('#slike_mali').animate({opacity: '0.2'}).slideUp('slow');
+		  $('#slike_dvoriste').animate({opacity: '0.2'}).slideUp('slow');
+		  $('#slike_novalja').animate({opacity: '0.2'}).slideUp('slow');
+
+		  $('#slike_veliki').slideDown('slow').animate({opacity: '0.8'});
+		});
+		}); 
+		$(document).ready(function sakrij(){
+		  $('#klik_dvoriste').click(function() {
+		  $('#slike_svi').animate({opacity: '0.2'}).slideUp('slow');
+		  $('#slike_mali').animate({opacity: '0.2'}).slideUp('slow');
+		  $('#slike_veliki').animate({opacity: '0.2'}).slideUp('slow');
+		  $('#slike_novalja').animate({opacity: '0.2'}).slideUp('slow');
+
+		  $('#slike_dvoriste').slideDown('slow').animate({opacity: '0.8'});
+		});
+		}); 
+		$(document).ready(function sakrij(){
+		  $('#klik_novalja').click(function() {
+		  $('#slike_svi').animate({opacity: '0.2'}).slideUp('slow');
+		  $('#slike_mali').animate({opacity: '0.2'}).slideUp('slow');
+		  $('#slike_veliki').animate({opacity: '0.2'}).slideUp('slow');
+		  $('#slike_dvoriste').animate({opacity: '0.2'}).slideUp('slow');
+
+		  $('#slike_novalja').slideDown('slow').animate({opacity: '0.8'});
+		});
+		}); 
+		
+// označavanje aktivne navigacije u galeriji
+
+$(document).ready(function(){
+	$('#gallery_nav li ').click(function(e) {
+        e.preventDefault();
+        $('li').removeClass('active');
+        $(this).addClass('active');
+    });
+	
+});
 
